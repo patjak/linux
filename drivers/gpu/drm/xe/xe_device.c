29b89e9a2eb1af66f5866b6d3812dea7528b6883
Lucas De Marchi <lucas.demarchi@intel.com>
2025-02-21 16:10:49 -0800

    drm/xe: Stop ignoring errors from xe_heci_gsc_init()
    Do not ignore errors from xe_heci_gsc_init(). For example, it shouldn't
    be fine to report successfully entering survivability mode when there's
    no communication with gsc working. The driver should also not be
    half-initialized in the normal case neither.
    
    Cc: Riana Tauro <riana.tauro@intel.com>
    Cc: Alexander Usyskin <alexander.usyskin@intel.com>
    Reviewed-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250222001051.3012936-10-lucas.demarchi@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -859,7 +859,9 @@ int xe_device_probe(struct xe_device *xe)
 			return err;
 	}
 
-	xe_heci_gsc_init(xe);
+	err = xe_heci_gsc_init(xe);
+	if (err)
+		return err;
 
 	err = xe_oa_init(xe);
 	if (err)
@@ -911,8 +913,6 @@ void xe_device_remove(struct xe_device *xe)
 	xe_display_unregister(xe);
 
 	drm_dev_unplug(&xe->drm);
-
-	xe_heci_gsc_fini(xe);
 }
 
 void xe_device_shutdown(struct xe_device *xe)
